7172e5ba120888eac433f2df1e0e727d66b314ce
“suyantmaharaj” <mhrsuy003@myuct.ac.za>
2026-06-24 14:02:15 +0200

    fixing favicon

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -21,7 +21,6 @@ export const metadata: Metadata = {
   authors:     [{ name: 'Sanlam Investments' }],
   icons: {
     icon: [
-      { url: '/favicon.ico', sizes: 'any' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
     ],
